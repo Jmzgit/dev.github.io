c1fc3fe0165f6c449485040c6198b84a955c75bf
son2o3 <aureliojmz@gmail.com>
2021-02-26 13:37:49 -0800

    removed minified js

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,1 +1,6 @@
-var myPix=["img/PrettyPerfectHerPink.webp","img/Pinkywinona.webp","img/PrincessPink.webp","img/VioletSplash.webp","img/pinkypromisedtokeepitplatonic.webp","img/coolfruitherjuice.webp","img/Bluewithouther.webp","img/niceorangeyellowsmiles.webp","img/PrettyPerfectHerPink.webp","img/Pinkywinona.webp","img/PrincessPink.webp","img/VioletSplash.webp"];setInterval(function walkPix(){const a=myPix.length;const b=Math.floor(Math.random()*a-0);document.getElementById("smallBlock").src=myPix[b]},3700);
+var myPix = ["img/PrettyPerfectHerPink.webp", "img/Pinkywinona.webp", "img/PrincessPink.webp", "img/VioletSplash.webp", "img/pinkypromisedtokeepitplatonic.webp", "img/coolfruitherjuice.webp", "img/Bluewithouther.webp", "img/niceorangeyellowsmiles.webp", "img/PrettyPerfectHerPink.webp", "img/Pinkywinona.webp", "img/PrincessPink.webp", "img/VioletSplash.webp"];
+setInterval(function walkPix() {
+   const a = myPix.length;
+   const b = Math.floor(Math.random() * a - 0);
+   document.getElementById("smallBlock").src = myPix[b]
+}, 3700);
