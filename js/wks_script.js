6d9cc9021bdedd30919dcfa13238edfc0461d6d7
son2o3 <aureliojmz@gmail.com>
2021-02-26 13:39:10 -0800

    yes

--- js/wks_script.js
+++ js/wks_script.js
@@ -1,6 +1,1 @@
-var myPix = ["img/PrettyPerfectHerPink.webp", "img/Pinkywinona.webp", "img/PrincessPink.webp", "img/VioletSplash.webp", "img/pinkypromisedtokeepitplatonic.webp", "img/coolfruitherjuice.webp", "img/Bluewithouther.webp", "img/niceorangeyellowsmiles.webp", "img/PrettyPerfectHerPink.webp", "img/Pinkywinona.webp", "img/PrincessPink.webp", "img/VioletSplash.webp"];
-setInterval(function walkPix() {
-   const a = myPix.length;
-   const b = Math.floor(Math.random() * a - 0);
-   document.getElementById("smallBlock").src = myPix[b]
-}, 3700);
+var myPix=["img/PrettyPerfectHerPink.webp","img/Pinkywinona.webp","img/PrincessPink.webp","img/VioletSplash.webp","img/pinkypromisedtokeepitplatonic.webp","img/coolfruitherjuice.webp","img/Bluewithouther.webp","img/niceorangeyellowsmiles.webp","img/PrettyPerfectHerPink.webp","img/Pinkywinona.webp","img/PrincessPink.webp","img/VioletSplash.webp"];setInterval(function walkPix(){const d=myPix.length;const c=Math.floor(Math.random()*d-0);document.getElementById("smallBlock").src=myPix[c]},3700);
